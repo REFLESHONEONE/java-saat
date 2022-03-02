1e1b9bc4cd10b5456ded8f3a65fbdb3b5a55533b
BİLAL <99627537+REFLESHONEONE@users.noreply.github.com>
2022-03-02 06:02:15 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+let java = prompt("please Are you writing name ?");
+
+let info = document.querySelector("#info");
+info.innerHTML = ` <img src="img/2_TZeK0kyHTRHVv3gUi8BtQg.png" alt="" />
+  <p > MERHABA ,  ${java} ! HOŞ GELDİN !</p>
+  <p> <strong>15:08:25</strong>  CUMA</p> 
+  <p><small>tarihinde</small>  frontend web development patikasının javascript progmanına katıldınız.
+</br> 1.bolum odevini yapıyorsunuz?</p>`;
+console.log(java);
